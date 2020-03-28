b3aae16298308f158bf22721492a82ac9c26e4b0
Tiffany Casey <TiffanyLynneCasey@gmail.com>
2020-03-28 12:37:32 -0400

    AJAX call now working

--- recipe.js
+++ recipe.js
@@ -1,1 +1,19 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+  console.log( "ready!" );
+});
 
+$("#find-recipe").on("click", function(event) {
+  event.preventDefault();
+  var ingredients = $(this).attr("#ingredient-input");
+
+var queryURL =
+  'https://api.spoonacular.com/recipes/findByIngredients?ingredients=' +
+  'tomato,cilantro' + 
+  '&apiKey=32de3e49a74748bd9db408e1bee255d2';
+  
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+});

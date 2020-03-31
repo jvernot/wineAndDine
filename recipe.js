8e6ec3fb1e1276d4aa9b9875c5e0f1d0fcf0c851
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-03-30 20:04:52 -0400

    added updated css styling, added in script links for materialize, updated inputs on index and working on ajax

--- recipe.js
+++ recipe.js
@@ -1,19 +1,34 @@
-// A $( document ).ready() block.
 $( document ).ready(function() {
-  console.log( "ready!" );
-});
+  console.log("ready!");
 
-$("#find-recipe").on("click", function(event) {
-  event.preventDefault();
-  var ingredients = $(this).attr("#ingredient-input");
+  // Event listener for all button elements
+  $("#ingredient-input").on("click", function(event) {
+    event.preventDefault();
 
-var queryURL =
-  'https://api.spoonacular.com/recipes/findByIngredients?ingredients=' +
-  'tomato,cilantro' + 
-  '&apiKey=32de3e49a74748bd9db408e1bee255d2';
+    var ingredients = $(this).attr("#ingredient-input");
   
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-});
+    var queryURL =
+      'https://api.spoonacular.com/recipes/findByIngredients?ingredients=' +
+    "chicken" + 
+      '&apiKey=32de3e49a74748bd9db408e1bee255d2';
+    
+    // Performing our AJAX GET request
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+
+      // After data comes back from the request
+  .then(function(response) {
+  console.log(queryURL);
+  console.log(response);
+
+  $("#recipe-view").text(JSON.stringify(response));
+
+  function getsource(id) {
+    success:function(res){
+      document.getElementById("recipeImage").innerHTML=res.sourceUrl
+      document.getElementById("sourcelink").href=res.source.Url
+    }
+    
+  });

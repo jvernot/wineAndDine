81536e8ebf608a95cfd8ad5136a498eff21b5fb3
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-04-04 07:22:56 -0400

    updated dine in html withheader, changed start over to main page, changed ingredient input to dropdown, updated JS

--- assets/JS/dine-in.js
+++ assets/JS/dine-in.js
@@ -2,50 +2,85 @@ $(document).ready(function() {
   console.log("ready!");
 
 
+  // Query the API
+  function buildQueryURL() {
+
+  // QueryURL to Spoonacular App 
+  var queryURL =  "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" +
+  recipeInput + 
+  "&apiKey=32de3e49a74748bd9db408e1bee255d2";
+
+  // Grabs the input from the ingredint input text
+  var recipeInput = $("#icon_prefix2").val();
+    console.log(recipeInput, "recipeInput");
+  
+  }
+
+  // Function that accounts for how many recipes they want (input or dropdown)
+  function updatedRecipes(recipeData) {
+   // Get from the form the number of results to display    
+          
+    var numRecipes = $("#ingredient-count").val();
+    console.log(recipeData); 
+
+    // Loop through and build elements for the defined number of recipes
+    for (var i = 0; i < numRecipes; i++) {
+    // Get specific article info for current index
+    var recipe = recipeData.response.docs[i];
+
+    // Increase the recipeCount (track recipe # - starting at 1)
+    var recipeCount = i + 1;
+
+     // Create the list group to contain the recipes and add the recipe content for each
+     var recipeList = $("<ul>");
+     recipeList.addClass("list-group");
+
+    }
+
+      // Prepending the image to the page  // Add the newly created element to the DOM
+    $("#recipe-view").append(recipeList);
+
+    // If the article has a headline, log and append to articleList
+    var title = recipe.title;
+    var recipeListItem = $("<li class='list-group-item recipeTitle'>");
+
+    if (title && title.main) {
+      console.log(title.main);
+      recipeListItem.append(
+        "<span class='label label-primary'>" +
+          recipeCount +
+          "</span>" +
+          "<strong> " +
+          title.main +
+          "</strong>"
+      );
+    }
+  }
+
+
+// CLICK HANDLER
+// ==========================================================
+
   // On-click when user enters in the ingredients they have on hand 
   $("#ingredient-input").on("click", function(event) {
     event.preventDefault();
 
-    // Grabs the input from the ingredint input text
-    var recipeInput = $("#icon_prefix2").val();
-    console.log(recipeInput, "recipeInput");
-
-    // QueryURL to Spoonacular App 
-    var queryURL =  "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" +
-    recipeInput + 
-    "&apiKey=32de3e49a74748bd9db408e1bee255d2"
+  // Build the query URL for the ajax request to the NYT API
+  var queryURL = buildQueryURL();
 
     //AJAX call 
     $.ajax({
-    url: queryURL,
-    method: "GET"
-    }).then(function(response) {
-        console.log(response[0].title);
-        console.log(response[0].image);
-        console.log(response[0].usedIngredients);
+      url: queryURL,
+      method: "GET"
+      }).then(updatedRecipes) 
+      console.log(updatedRecipes[0].title); 
+  });     
 
-          
+}); 
 
-      // Function that accounts for how many recipes they want (input or dropdown)
-      $("#ingredient-amount").on("click", function(event) {
-        event.preventDefault();
 
-        //defining how many articles they want 
-        var amountInput = $("#icon_prefix2").val();
-        console.log(amountInput, "amountInput");
-      }) // ingredient amount end response 
 
-        // Loop through and build elements for the defined number of recipes
-        //for (var i = 0; i < numRecipes; i++) {
-        // Get specific article info for current index
-         // var article = response[i];
+  
 
-        // Increase the articleCount (track article # - starting at 1)
-        //var recipeCount = i + 1;
 
-      // Prepending the image to the page 
-      $("#recipe-view").append(response[0].title);
-       
-    }) // End of then response
-  }) //  End of Ingredient-input on click
-}); // Final closing tag
+
